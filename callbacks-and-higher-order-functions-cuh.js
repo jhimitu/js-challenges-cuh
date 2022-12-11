f8c933c343b49bee5c5e8255dc6b6a5ef75f9c05
Jhia Turner <jhimitu@gmail.com>
2022-12-11 10:16:00 -0800

    complete challenge 16

--- callbacks-and-higher-order-functions-cuh.js
+++ callbacks-and-higher-order-functions-cuh.js
@@ -322,20 +322,28 @@ function groupBy(array, callback) {
 // console.log(groupBy(decimals, floored)); // should log: { 1: [1.3], 2: [2.1, 2.4] }
 
 
-// Challenge 16
+// Challenge 16 ✅
 /*
 Create a function called goodKeys that accepts an object and a callback. The callback will return either true or false. 
 goodKeys will iterate through the object and perform the callback on each value. goodKeys will then return an array 
 consisting only the keys whose associated values yielded a true return value from the callback.
 */
 function goodKeys(obj, callback) {
+    const trueVals = [];
 
+    for (const key in obj) {
+        if (callback(obj[key])) {
+            trueVals.push(key);
+        }
+    }
+
+    return trueVals;
 }
 
 // /*** Uncomment these to check your work! ***/
-// const sunny = { mac: 'priest', dennis: 'calculating', charlie: 'birdlaw', dee: 'bird', frank: 'warthog' };
-// const startsWithBird = function(str) { return str.slice(0, 4).toLowerCase() === 'bird'; };
-// console.log(goodKeys(sunny, startsWithBird)); // should log: ['charlie', 'dee']
+const sunny = { mac: 'priest', dennis: 'calculating', charlie: 'birdlaw', dee: 'bird', frank: 'warthog' };
+const startsWithBird = function(str) { return str.slice(0, 4).toLowerCase() === 'bird'; };
+console.log(goodKeys(sunny, startsWithBird)); // should log: ['charlie', 'dee']
 
 
 // Challenge 17
